ea12177d07c4085cbf273d377c9d13e504cc44c5
wiaqen0 <wia.wia.164@gmail.com>
2023-05-15 10:16:25 +0700

    add staticfiles v2

--- staticfiles/order.js
+++
@@ -1,6 +0,0 @@
-function modPrice() {
-    var price = document.getElementById("price").innerText;
-    var quantity = document.getElementById("quantity").value;
-    var total = parseInt(price) * parseInt(quantity);
-    document.getElementById("total_price").innerHTML = "Total price: $" + total;
-}
