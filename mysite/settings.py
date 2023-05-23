7adbe793d77994b891430596691e4ba8b4836f0c
wiaqen0 <wia.wia.164@gmail.com>
2023-05-23 11:50:33 +0700

    khong biet cai gi nua

--- mysite/settings.py
+++ mysite/settings.py
@@ -102,6 +102,12 @@ DATABASES = {
     }
 }
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': 'db.sqlite3',
+#     }
+# }
 
 # Password validation
 # https://docs.djangoproject.com/en/4.0/ref/settings/#auth-password-validators
