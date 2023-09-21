1bac4b1453fb49ca5f64866e8dac473abe09dab7
wiaqen0 <wia.wia.164@gmail.com>
2023-09-21 09:48:26 +0700

    change db

--- mysite/settings.py
+++ mysite/settings.py
@@ -92,22 +92,22 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'calc',
-        'USER': 'postgres',
-        'PASSWORD': 'nghia',
-        'HOST': 'localhost'
-    }
-}
-
 # DATABASES = {
 #     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': 'db.sqlite3',
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': 'calc',
+#         'USER': 'postgres',
+#         'PASSWORD': 'nghia',
+#         'HOST': 'localhost'
 #     }
 # }
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/4.0/ref/settings/#auth-password-validators
